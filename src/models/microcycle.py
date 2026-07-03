5969c44238fea6e4e99cc971a887671106ebd655
angelrb <angelramibla@gmail.com>
2026-07-03 19:41:14 +0200

    Add JSON serialization to Microcycle
    Add `from_json()` and `to_json()` methods to enable serialization/deserialization of Microcycle instances. Make the `workouts` parameter optional in the constructor, allowing it to be passed during initialization or defaulting to an empty list.

--- src/models/microcycle.py
+++ src/models/microcycle.py
@@ -1,8 +1,23 @@
 class Microcycle:
-    def __init__(self, week_index, length):
+    def __init__(self, week_index, length, workouts = None):
         self.week = week_index
         self.length = length
-        self.workouts = [None * length]
+        self.workouts = workouts if workouts else [None * length]
     
+    def __repr__(self):
+        return f"<Microcycles {self.name}>"
+
+    @classmethod
+    def from_json(cls, data_dict):
+        return cls(week_index=data_dict["week"], length=data_dict["length"], workouts=data_dict["workouts"])
+    
+    def to_json(self):
+        return {
+            "week": self.week,
+            "length": self.length,
+            "workouts": self.workouts
+        }
+        
+
     def add_workout(self, workout, day_index):
         self.workouts[day_index] = workout
