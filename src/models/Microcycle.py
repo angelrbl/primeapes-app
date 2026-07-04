237d1a37e09601a6f172e2a9364036b49c02a679
angelrb <angelramibla@gmail.com>
2026-07-04 21:30:00 +0200

    Add ID tracking and JSON serialization to models
    Add microcycle ID tracking to Microcycle model. Implement proper JSON serialization with from_json() and to_json() methods for both Microcycle and Macrocycle, using object mapping to handle relationships. Add __repr__() method to Macrocycle and fix typo in Microcycle's __repr__().

--- src/models/Microcycle.py
+++ src/models/Microcycle.py
@@ -1,18 +1,22 @@
 class Microcycle:
-    def __init__(self, week_index, length, workouts = None):
+    def __init__(self, microcycle_id, week_index, length, workouts = None):
+        self.id = microcycle_id
         self.week = week_index
         self.length = length
         self.workouts = workouts if workouts else [None * length]
     
     def __repr__(self):
-        return f"<Microcycles {self.name}>"
+        return f"<Microcycle {self.name}>"
 
     @classmethod
-    def from_json(cls, data_dict):
-        return cls(week_index=data_dict["week"], length=data_dict["length"], workouts=data_dict["workouts"])
+    def from_json(cls, data_dict, workout_map):
+        workout_names = data_dict["workouts"]
+        workouts = [workout_map[name] for name in workout_names]
+        return cls(microcycle_id=data_dict["id"], week_index=data_dict["week"], length=data_dict["length"], workouts=workouts)
     
     def to_json(self):
         return {
+            "id": self.id,
             "week": self.week,
             "length": self.length,
             "workouts": self.workouts
@@ -20,3 +24,6 @@ class Microcycle:
 
     def add_workout(self, workout, day_index):
         self.workouts[day_index] = workout
+
+    def get_id(self):
+        return self.id
--- src/models/macrocycle.py
+++ src/models/macrocycle.py
@@ -1,9 +1,26 @@
 class Macrocycle:
-    def __init__(self, name, start_date, length):
+    def __init__(self, name, start_date, length, microcycles = None):
         self.name = name
         self.start_date = start_date
         self.length = length
-        self.microcycles = [None * length]
+        self.microcycles = microcycles if microcycles else [None * length]
+
+    def __repr__(self):
+        return f"<Macrocycle {self.name}>"
+
+    @classmethod
+    def from_json(cls, data_dict, microcycle_map):
+        microcycle_ids = data_dict["microcycles"]
+        microcycles = [microcycle_map[microcycle_id] for microcycle_id in microcycle_ids]
+        return cls(name=data_dict["name"], start_date=data_dict["start_date"], length=data_dict["length"], microcycles=microcycles)
+    
+    def to_json(self):
+        return {
+            "name": self.name,
+            "start_date": self.start_date,
+            "length": self.length,
+            "microcycles": [microcycle.get_id() for microcycle in self.microcycles]
+        }
 
     def add_microcycle(self, microcycle_index, microcycle):
         self.microcycles[microcycle_index] = microcycle
